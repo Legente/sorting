ae7f752fac6fa227a44998ce4273209c2e2b1f61
Legente <williamvernillo@gmail.com>
2022-07-11 11:17:22 +0200

    Solve code smells

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,24 +7,26 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str;
+
         List<String> names = new ArrayList<>();
         do {
-			System.out.println("Please Enter a name (Write 'sort' to sort the list) : ");
+            Log.info("Please Enter a name (Write 'sort' to sort the list) : ");
             str = sc.nextLine();
             if (!"sort".equals(str)) {
                 names.add(str);
-                System.out.println("Names entered: " + names);
+                Log.info("Names entered: " + names);
             }
         } while (!"sort".equals(str));
 
         sort(names);
 
-		System.out.println("Names sorted: " + names);
+        Log.info("Names sorted: " + names);
     }
 
 
     public static void sort(List<String> names) {
         names.sort(Collections.reverseOrder());
     }
+
 
 }
